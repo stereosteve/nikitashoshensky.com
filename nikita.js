591d5639efe7f0629a3f727fcad335b8715ef1b5
Steve Perkins <steve@indabamusic.com>
2012-06-27 07:55:17 -0400

    Adding detail shots

--- nikita.js
+++ nikita.js
@@ -1,14 +1,47 @@
 /*globals $:true*/
 
 var WORKS = [
-  { title: 'Howdy',
-    thumb: '1-nikita-shoshensky.jpg' },
-  { title: 'mommy',
-    thumb: '2-nikita-shoshensky.jpg' },
+  {
+    title: 'The Professionals I',
+    thumb: 'professionals-thumb.jpg',
+    detail: [
+      'The-Professionals.jpg',
+      'The-professionals-detail.jpg',
+      ]
+  },
+  {
+    title: 'Chico',
+    thumb: 'ms1-thumb.jpg',
+    detail: [
+      'ms1.jpg',
+      'ms2.jpg',
+      'ms3.jpg',
+      'ms4.jpg',
+      'ms5.jpg',
+      'ms6.jpg',
+      'ms7.jpg',
+      ]
+  },
+  {
+    title: 'mommy',
+    thumb: '2-nikita-shoshensky.jpg',
+  },
   { title: 'daddy',
     thumb: '3-shoshensky-nikita.jpg' },
 ]
 
-function ThumbsCtrl($scope) {
+function AppCtrl($scope) {
   $scope.works = WORKS
+
+  $scope.showWork = function(work) {
+    $scope.detail = work
+    $scope.detailIndex = 0
+    $scope.detailImg = work.detail[$scope.detailIndex]
+  }
+
+  $scope.next = function() {
+    $scope.detailIndex += 1
+    $scope.detailImg = $scope.detail.detail[$scope.detailIndex]
+  }
+
 }
