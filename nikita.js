10be4c465bd12438bed28aab0889d2825bc97559
Steve Perkins <steve@indabamusic.com>
2012-06-27 08:54:15 -0400

    jQuery and ColorBox - so painful.

--- nikita.js
+++ nikita.js
@@ -1,19 +1,19 @@
-/*globals $:true*/
+/*globals $:true _:true*/
 
 var WORKS = [
   {
     title: 'The Professionals I',
     thumb: 'professionals-thumb.jpg',
-    detail: [
-      'The-Professionals.jpg',
+    detail: 'The-Professionals.jpg',
+    more: [
       'The-professionals-detail.jpg',
       ]
   },
   {
     title: 'Chico',
     thumb: 'ms1-thumb.jpg',
-    detail: [
-      'ms1.jpg',
+    detail: 'ms1.jpg',
+    more: [
       'ms2.jpg',
       'ms3.jpg',
       'ms4.jpg',
@@ -25,23 +25,36 @@ var WORKS = [
   {
     title: 'mommy',
     thumb: '2-nikita-shoshensky.jpg',
-  },
-  { title: 'daddy',
-    thumb: '3-shoshensky-nikita.jpg' },
+    detail: '',
+    more: []
+  }
 ]
 
-function AppCtrl($scope) {
-  $scope.works = WORKS
 
-  $scope.showWork = function(work) {
-    $scope.detail = work
-    $scope.detailIndex = 0
-    $scope.detailImg = work.detail[$scope.detailIndex]
-  }
 
-  $scope.next = function() {
-    $scope.detailIndex += 1
-    $scope.detailImg = $scope.detail.detail[$scope.detailIndex]
-  }
+$(function() {
 
-}
+  var thumbTemplate = _.template($("#thumb_template").text())
+  var $thumbs = $(".thumbs")
+
+
+  WORKS.forEach(function(work, index) {
+    work.id = "work-" + index
+    var $thumb = $(thumbTemplate(work))
+    $thumb.data('work', work)
+    $thumbs.append($thumb)
+  })
+
+  $('.colorbox').colorbox({maxWidth: "80%", maxHeight: "80%"})
+
+
+  //$('body').on('click .thumb', function(ev) {
+    //var $thumb = $(ev.target)
+    //var work = $thumb.data('work')
+
+    //$.colorbox({rel: 
+  //})
+
+})
+
+window.WORKS = WORKS
